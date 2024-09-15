e190bea0adc93d1b21757e2c765c2fb51f963d39
mepatrick73 <pameu17@ulaval.ca>
2024-09-15 16:47:27 -0400

    window is now created

---
+++ src/app.rs
@@ -0,0 +1,34 @@
+use winit::{application::ApplicationHandler, event::WindowEvent, window::Window};
+
+#[derive(Default)]
+pub struct DataVisApp {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for DataVisApp {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use data_vis_app::app::DataVisApp;
+use winit::event_loop::{ControlFlow, EventLoop};
+
+fn main() -> Result<()> {
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = DataVisApp::default();
+    event_loop.run_app(&mut app)?;
+    Ok(())
 }
